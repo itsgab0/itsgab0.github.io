3a6e03bbe55ed0ed17de688d32b71f3f1b44e4d9
itsgab0 <40537815+itsgab0@users.noreply.github.com>
2022-08-25 16:07:13 +0100

    Add files via upload

---
+++ Lingo/script.js
@@ -0,0 +1,5 @@
+var x = document.getElementsByClassName("entry").length;
+
+console.log(x);
+
+document.getElementById("numTerms").innerHTML += ( "<b>" + x + "</b>");
---
+++ MorphemeMaps/script.js
@@ -0,0 +1,56 @@
+//console.log(data);
+
+function getText (i) {
+  
+  var strReturn = "";
+  
+  for ( let j = 0; j < data[i].native.length; j++ ) {
+    
+    strReturn += "<span style='color:";
+    strReturn += colours[ j % colours.length ];
+    strReturn += ";'>";
+    strReturn += data[i].native[j];
+    strReturn += "</span>";
+    
+  }
+  
+  if( data[i].hasOwnProperty('transliteration') ) {
+    
+    strReturn += "</td></tr><tr style='background-color: white;'><td colspan='3'>";
+    
+    for ( let j = 0; j < data[i].transliteration.length; j++ ) {
+ 
+      strReturn += "<span style='color:";
+      strReturn += colours[ j % colours.length ];
+      strReturn += ";'>";
+      strReturn += data[i].transliteration[j];
+      strReturn += "</span>";
+    
+    }
+    
+  }
+  
+  strReturn += "<hr style='width: 90%;'>";
+  
+  strReturn += "</td></tr><tr style='background-color: white;'><td colspan='3'>";
+  
+  for ( let j = 0; j < data[i].english.length; j++ ) {
+    
+    strReturn += "<span style='color:";
+    strReturn += colours[ data[i].englishMap[j] % colours.length ];
+    strReturn += ";'>";
+    strReturn += data[i].english[j] + "</span>";
+    
+  }
+  
+  return strReturn;
+  
+}
+
+const colours = ["red", "orange", "lime", "green", "tomato", "blue", "maroon", "fuchsia", "blueviolet", "orchid", "purple", "seagreen", "black", "darksalmon", "teal", "palegreen", "indigo"];
+
+for ( let i = 0; i < data.length; i++ ) {
+  
+  document.getElementById("morphemeMapsTableBlock").innerHTML += ( "<table><tr><th style='text-align: left; padding-left: 8px;'>" + data[i].code.toUpperCase() + "</th><th style='padding-left: 8px;'>" + data[i].language + "</th><th style='text-align: right; padding-right: 8px;'>" + data[i].creator + "</th></tr><tr style='background-color: white;'><td colspan='3'>" + getText(i) + "</td></tr></table>");
+  
+}
