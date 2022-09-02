1236be7e8ecefa6b4a476c93d18974f030221ca5
itsgab0 <gabefoad@gmail.com>
2022-09-02 12:43:00 +0100

    Added top-scroll button

--- Lingo/script.js
+++ Lingo/script.js
@@ -1,5 +1,31 @@
-var x = document.getElementsByClassName("entry").length;
+var x = document.getElementsByClassName( "entry" ).length;
 
 console.log(x);
 
-document.getElementById("numTerms").innerHTML += ( "<b>" + x + "</b>");
+document.getElementById( "numTerms" ).innerHTML += ( "<b>" + x + "</b>");
+
+var topButton = document.getElementById( "topButton" );
+
+window.onscroll = function() { scrollListener() };
+
+function scrollListener() {
+  
+  if( document.body.scrollTop > 32 || document.documentElement.scrollTop > 32 ) {
+    
+    topButton.style.display = "block";
+    
+  } else {
+    
+    topButton.style.display = "none";
+    
+  }
+  
+}
+
+function goTop() {
+  
+  document.body.scrollTop = 0;
+  
+  document.documentElement.scrollTop = 0;
+  
+}
