9ca391a64b35f4e85891d5dbeca28dd8598d635d
itsgab0 <gabefoad@gmail.com>
2022-09-08 23:23:32 +0100

    Reformatted Entire Site

--- Lingo/script.js
+++ Lingo/script.js
@@ -3,29 +3,3 @@ var x = document.getElementsByClassName( "entry" ).length;
 console.log(x);
 
 document.getElementById( "numTerms" ).innerHTML += ( "<b>" + x + "</b>");
-
-var topButton = document.getElementById( "topButton" );
-
-window.onscroll = function() { scrollListener() };
-
-function scrollListener() {
-  
-  if( document.body.scrollTop > 32 || document.documentElement.scrollTop > 32 ) {
-    
-    topButton.style.display = "block";
-    
-  } else {
-    
-    topButton.style.display = "none";
-    
-  }
-  
-}
-
-function goTop() {
-  
-  document.body.scrollTop = 0;
-  
-  document.documentElement.scrollTop = 0;
-  
-}
---
+++ globalscript.js
@@ -0,0 +1,25 @@
+var topButton = document.getElementById( "topButton" );
+
+window.onscroll = function() { scrollListener() };
+
+function scrollListener() {
+  
+  if( document.body.scrollTop > 32 || document.documentElement.scrollTop > 32 ) {
+    
+    topButton.style.display = "block";
+    
+  } else {
+    
+    topButton.style.display = "none";
+    
+  }
+  
+}
+
+function goTop() {
+  
+  document.body.scrollTop = 0;
+  
+  document.documentElement.scrollTop = 0;
+  
+}
